54e7c5cfbcbf18daf0243b7a5cb4bb00943457bb
mkojoa <michael.ameyaw@persol.net>
2024-01-11 20:47:00 +0000

    check in

--- evat-workflow/Program.cs
+++ evat-workflow/Program.cs
@@ -15,6 +15,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 
+var tt = builder.Configuration["IdpSettings:Authority"]; ;
+
 builder.Services.AddAuthentication(options =>
 {
     options.DefaultAuthenticateScheme = IdentityServerAuthenticationDefaults.AuthenticationScheme;
