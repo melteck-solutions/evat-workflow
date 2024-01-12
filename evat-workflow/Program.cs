894870cfda2c83896cb39b6020c2cceaaceabbd2
mkojoa <michael.ameyaw@persol.net>
2024-01-12 16:31:24 +0000

    minor changes with fine update

--- evat-workflow/Program.cs
+++ evat-workflow/Program.cs
@@ -2,7 +2,18 @@
 using evat_workflow;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var env = builder.Environment;
+
+builder.Configuration
+	.SetBasePath(Directory.GetCurrentDirectory())
+	.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+	.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+	.AddEnvironmentVariables();
+
 var startup = new Startup(builder.Configuration);
 startup.ConfigureServices(builder.Services); // calling ConfigureServices method
+
+
 var app = builder.Build();
 startup.Configure(app, builder.Environment); // calling Configure method
